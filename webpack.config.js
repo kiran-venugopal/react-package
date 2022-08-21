9c18f65d295a0b38e22efee4e828d3b6157b90e0
Kiran V <kiranvenugopal07@gmail.com>
2022-08-21 11:51:07 +0530

    updated react

--- webpack.config.js
+++ webpack.config.js
@@ -13,14 +13,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules)/,
-        use: "babel-loader",
-      },
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: ["ts-loader"],
+        test: /\.tsx?$/,
+        loader: "esbuild-loader",
+        options: {
+          loader: "tsx", // Or 'ts' if you don't need tsx
+          target: "es2015",
+        },
       },
       {
         test: /\.css$/,
